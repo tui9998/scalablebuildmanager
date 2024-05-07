fc6fe7c58332c7cc64ab0f9719fffafb05556675
tui9998 <sixsense61pun0suy@hotmail.com>
2024-05-07 17:54:39 +0700

    update aptos

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-function coinChange(coins, amount) {
-  const dp = new Array(amount + 1).fill(amount + 1);
-  dp[0] = 0;
-  for (const coin of coins) {
-    for (let i = coin; i <= amount; i++) {
-      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+function canCross(stones) {
+  const map = new Map();
+  for (const stone of stones) map.set(stone, new Set());
+  map.get(0).add(0);
+  for (let i = 0; i < stones.length; i++) {
+    for (const step of map.get(stones[i])) {
+      for (let k = step - 1; k <= step + 1; k++) {
+        if (k > 0 && map.has(stones[i] + k)) map.get(stones[i] + k).add(k);
+      }
     }
   }
-  return dp[amount] > amount ? -1 : dp[amount];
+  return map.get(stones[stones.length - 1]).size > 0;
 }
